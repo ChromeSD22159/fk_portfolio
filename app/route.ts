984c4a8d1a0c5637aeae85598ee5f2b964704889
Frederik Kohler <info@frederikkohler.de>
2025-12-27 20:51:08 +0100

    BaseDomain update

--- app/route.ts
+++ app/route.ts
@@ -3,10 +3,10 @@ import { NextRequest, NextResponse } from "next/server";
 export function GET(req: NextRequest) {
   const acceptLanguage = req.headers.get("accept-language") || "en";
   const baseLang = acceptLanguage.split(",")[0].split("-")[0];
-
+  const baseDomain = "www.frederikkohler.de"
   if (["de", "at", "ch"].includes(baseLang)) {
-    return NextResponse.redirect(new URL("/de", req.url));
+    return NextResponse.redirect(new URL(baseDomain + "/de", req.url));
   }
 
-  return NextResponse.redirect(new URL("/en", req.url));
+  return NextResponse.redirect(new URL(baseDomain + "/en", req.url));
 }
