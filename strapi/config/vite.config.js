56627006793b4c96218dca8980b6fedf8726e0d8
Frederik Kohler <info@frederikkohler.de>
2026-01-01 23:46:16 +0100

    update

--- strapi/config/vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite';
-
-export default defineConfig({
-  server: {
-    allowedHosts: ['frederikkohler.de', 'www.frederikkohler.de', 'localhost', '69.62.113.185'],
-  },
-});
